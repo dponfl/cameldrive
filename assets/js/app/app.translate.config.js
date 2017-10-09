92d198b6a2ca9e442655eb630602608825f823f1
dmsch <dschukin@mail.ru>
2017-10-09 21:59:53 +0300

    home: section 01

--- assets/js/app/app.translate.config.js
+++ assets/js/app/app.translate.config.js
@@ -50,13 +50,6 @@ function translateConfig ($translateProvider) {
     'NAV_CONTACTS': 'Contacts',
     'NAV_REQ_BUTTON': 'Make request',
 
-    // Request button dropdown menu items
-    'NAV_REQ_DAILY': 'Daily rent',
-    'NAV_REQ_LONG_TERM': 'Long term rent',
-    'NAV_REQ_LET_PROP': 'Let property',
-    'NAV_REQ_BUY_PROP': 'Buy property',
-    'NAV_REQ_SELL_PROP': 'Sell property',
-
     // Filter form items
     'PANEL_HEADER': 'Choose',
     'LBL_DEAL': 'Deal type',
@@ -98,37 +91,9 @@ function translateConfig ($translateProvider) {
     'HEADER_INFO_MODAL_FOOTER_CLOSE_BUTTON': 'Close',
 
 
-    // Home - Carousel
-    'HOME_CAROUSEL_TITLE_1': 'Slide 01 message',
-    'HOME_CAROUSEL_TEXT_1': 'Text for Slide 01',
-    'HOME_CAROUSEL_TITLE_2': 'Slide 02 message',
-    'HOME_CAROUSEL_TEXT_2': 'Text for Slide 02',
-    'HOME_CAROUSEL_TITLE_3': 'Slide 03 message',
-    'HOME_CAROUSEL_TEXT_3': 'Text for Slide 03',
-
     // Home - Main
-    'HOME_TITLE_H1': '[En]Недвижимость на Кипре',
-    'HOME_SUBTITLE': '[En]Несколько слов о том, что мы будем рады вам предложить...',
-    'HOME_PAGE_BODY_LEAD': '[En]Лудус утрояуе оцурререт ет мел, видит цонтентионес ин иус! ' +
-    'Яуот опортере те дуо, фабеллас цонсулату еум еи. Ех солеат пхилосопхиа при?',
-    'HOME_PAGE_BODY_P_1': '[En]Лорем ипсум долор сит амет, не дицит легере еос, еос еу волутпат цомпрехенсам, ' +
-    'не хас долор омиттам! При утамур сингулис адиписцинг не, не алии ехерци дуо. ' +
-    'Тота игнота алияуандо ан яуо, хабео яуидам хендрерит те яуо. Меи ерипуит фацилис ' +
-    'перицулис не, виси адиписци ад меи. Вел прима интегре те, вениам адверсариум вим цу. ' +
-    'Долорум мнесарчум диссентиунт иус не, но агам примис видерер нец. Еа поссе миним ' +
-    'дицерет сеа, усу менандри цонсулату еи.',
-    'HOME_PAGE_BODY_P_2': '[En]Лорем ипсум долор сит амет, не дицит легере еос, еос еу волутпат цомпрехенсам, ' +
-    'не хас долор омиттам! При утамур сингулис адиписцинг не, не алии ехерци дуо. ' +
-    'Тота игнота алияуандо ан яуо, хабео яуидам хендрерит те яуо. Меи ерипуит фацилис ' +
-    'перицулис не, виси адиписци ад меи. Вел прима интегре те, вениам адверсариум вим цу. ' +
-    'Долорум мнесарчум диссентиунт иус не, но агам примис видерер нец. Еа поссе миним ' +
-    'дицерет сеа, усу менандри цонсулату еи.',
-    'HOME_PAGE_BODY_P_3': '[En]Лорем ипсум долор сит амет, не дицит легере еос, еос еу волутпат цомпрехенсам, ' +
-    'не хас долор омиттам! При утамур сингулис адиписцинг не, не алии ехерци дуо. ' +
-    'Тота игнота алияуандо ан яуо, хабео яуидам хендрерит те яуо. Меи ерипуит фацилис ' +
-    'перицулис не, виси адиписци ад меи. Вел прима интегре те, вениам адверсариум вим цу. ' +
-    'Долорум мнесарчум диссентиунт иус не, но агам примис видерер нец. Еа поссе миним ' +
-    'дицерет сеа, усу менандри цонсулату еи.',
+    'HOME_TITLE': 'Camel Car Hire',
+    'HOME_SUBTITLE': 'You\'ll not be upset',
 
     // General
     'GENERAL_ELEM_OBJ': 'Object #',
@@ -137,59 +102,9 @@ function translateConfig ($translateProvider) {
     'GENERAL_FOUND_NOTHING': 'Nothing found. Change search criteria and try again.',
     'GENERAL_LOADING_SPINNER': 'Loading...',
 
-    // Exclusive
-    'EXCLUSIVE_PAGE_HEADER_TITLE': '[En]Эксклюзивные объекты',
-    'EXCLUSIVE_PAGE_HEADER_SUBTITLE': '[En]Несколько слов...',
-    'EXCLUSIVE_PAGE_BODY_H1': '[En]Эксклюзивные объекты недвижимости',
-    'EXCLUSIVE_PAGE_BODY_H2': '[En]Аренда вилл и аппартаментов на Кипре',
-    'EXCLUSIVE_PAGE_BODY_H3': '[En]Здесь представлена недвижимость, которая находится ' +
-    'в эксклюзивном управлении компании Orange Holliday villas.',
-    'EXCLUSIVE_PAGE_MORE_OBJ': '[En]Подробная информация об этих и других эксклюзивных объектах',
-    'EXCLUSIVE_PAGE_BUTTON_PRICE': 'Price',
-    'EXCLUSIVE_PAGE_PRICE_MODAL_HEADER': 'Price',
-    'EXCLUSIVE_PAGE_PRICE_MODAL_TABLE_HEAD_COL_1': 'Period',
-    'EXCLUSIVE_PAGE_PRICE_MODAL_TABLE_HEAD_COL_2': 'Price',
-    'EXCLUSIVE_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Close',
-    'EXCLUSIVE_PAGE_BUTTON_CALENDAR': 'Calendar',
-    'EXCLUSIVE_PAGE_CALENDAR_MODAL_HEADER': 'Calendar',
-    'EXCLUSIVE_PAGE_BUTTON_BOOK_OBJECT': 'Book this object',
-    'EXCLUSIVE_PAGE_BUTTON_YOUTUBE': 'See object in YouTube',
-    'EXCLUSIVE_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Object booking',
-
-    // Short term
-    'SHORTTERM_PAGE_HEADER_TITLE': '[En]Посуточная аренда',
-    'SHORTTERM_PAGE_HEADER_SUBTITLE': '[En]Несколько слов...',
-    'SHORTTERM_PAGE_MORE_OBJ': '[En]Подробная информация об этих и других объектах с посуточной арендой',
-    'SHORTTERM_PAGE_BUTTON_PRICE': 'Price',
-    'SHORTTERM_PAGE_PRICE_MODAL_HEADER': 'Price',
-    'SHORTTERM_PAGE_PRICE_MODAL_TABLE_HEAD_COL_1': 'Period',
-    'SHORTTERM_PAGE_PRICE_MODAL_TABLE_HEAD_COL_2': 'Price',
-    'SHORTTERM_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Close',
-    'SHORTTERM_PAGE_BUTTON_CALENDAR': 'Calendar',
-    'SHORTTERM_PAGE_CALENDAR_MODAL_HEADER': 'Calendar',
-    'SHORTTERM_PAGE_BUTTON_BOOK_OBJECT': 'Book this object',
-    'SHORTTERM_PAGE_BUTTON_YOUTUBE': 'See object in YouTube',
-    'SHORTTERM_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Object booking',
 
 
-    // Long term
-    'LONGTERM_PAGE_HEADER_TITLE': '[En]Долгосрочная аренда',
-    'LONGTERM_PAGE_HEADER_SUBTITLE': '[En]Несколько слов...',
-    'LONGTERM_PAGE_MORE_OBJ': '[En]Подробная информация об этих и других объектах с долгосрочной арендой',
-    'LONGTERM_PAGE_BUTTON_BOOK_OBJECT': 'Book this object',
-    'LONGTERM_PAGE_BUTTON_YOUTUBE': 'See object in YouTube',
-    'LONGTERM_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Object booking',
-    'LONGTERM_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Close',
 
-
-    // Sale
-    'SALE_PAGE_HEADER_TITLE': '[En]Продажа',
-    'SALE_PAGE_HEADER_SUBTITLE': '[En]Несколько слов...',
-    'SALE_PAGE_MORE_OBJ': '[En]Подробная информация об этих и других объектах продажи',
-    'SALE_PAGE_BUTTON_BOOK_OBJECT': 'Book this object',
-    'SALE_PAGE_BUTTON_YOUTUBE': 'See object in YouTube',
-    'SALE_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Object booking',
-    'SALE_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Close',
 
     // Q&A
     'QA_PAGE_HEADER_TITLE': '[En]Вопросы и ответы',
@@ -269,13 +184,6 @@ function translateConfig ($translateProvider) {
     'NAV_CONTACTS': 'Контакты',
     'NAV_REQ_BUTTON': 'Оставить заявку',
 
-    // Request button dropdown menu items
-    'NAV_REQ_DAILY': 'Снять недв. посуточно',
-    'NAV_REQ_LONG_TERM': 'Снять недв. долгосрочно',
-    'NAV_REQ_LET_PROP': 'Сдать недвижимость',
-    'NAV_REQ_BUY_PROP': 'Купить недвижимость',
-    'NAV_REQ_SELL_PROP': 'Продать недвижимость',
-
     // Filter form items
     'PANEL_HEADER': 'Задайте условия поиска',
     'LBL_DEAL': 'Тип сделки',
@@ -330,37 +238,9 @@ function translateConfig ($translateProvider) {
 
 
 
-    // Home - Carousel
-    'HOME_CAROUSEL_TITLE_1': 'Слайд 01',
-    'HOME_CAROUSEL_TEXT_1': 'Текст для слайда 01',
-    'HOME_CAROUSEL_TITLE_2': 'Слайд 02',
-    'HOME_CAROUSEL_TEXT_2': 'Текст для слайда 02',
-    'HOME_CAROUSEL_TITLE_3': 'Слайд 03',
-    'HOME_CAROUSEL_TEXT_3': 'Текст для слайда 03',
-
     // Home - Main
-    'HOME_TITLE_H1': 'Недвижимость на Кипре',
-    'HOME_SUBTITLE': 'Несколько слов о том, что мы будем рады вам предложить...',
-    'HOME_PAGE_BODY_LEAD': 'Лудус утрояуе оцурререт ет мел, видит цонтентионес ин иус! ' +
-    'Яуот опортере те дуо, фабеллас цонсулату еум еи. Ех солеат пхилосопхиа при?',
-    'HOME_PAGE_BODY_P_1': 'Лорем ипсум долор сит амет, не дицит легере еос, еос еу волутпат цомпрехенсам, ' +
-    'не хас долор омиттам! При утамур сингулис адиписцинг не, не алии ехерци дуо. ' +
-    'Тота игнота алияуандо ан яуо, хабео яуидам хендрерит те яуо. Меи ерипуит фацилис ' +
-    'перицулис не, виси адиписци ад меи. Вел прима интегре те, вениам адверсариум вим цу. ' +
-    'Долорум мнесарчум диссентиунт иус не, но агам примис видерер нец. Еа поссе миним ' +
-    'дицерет сеа, усу менандри цонсулату еи.',
-    'HOME_PAGE_BODY_P_2': 'Лорем ипсум долор сит амет, не дицит легере еос, еос еу волутпат цомпрехенсам, ' +
-    'не хас долор омиттам! При утамур сингулис адиписцинг не, не алии ехерци дуо. ' +
-    'Тота игнота алияуандо ан яуо, хабео яуидам хендрерит те яуо. Меи ерипуит фацилис ' +
-    'перицулис не, виси адиписци ад меи. Вел прима интегре те, вениам адверсариум вим цу. ' +
-    'Долорум мнесарчум диссентиунт иус не, но агам примис видерер нец. Еа поссе миним ' +
-    'дицерет сеа, усу менандри цонсулату еи.',
-    'HOME_PAGE_BODY_P_3': 'Лорем ипсум долор сит амет, не дицит легере еос, еос еу волутпат цомпрехенсам, ' +
-    'не хас долор омиттам! При утамур сингулис адиписцинг не, не алии ехерци дуо. ' +
-    'Тота игнота алияуандо ан яуо, хабео яуидам хендрерит те яуо. Меи ерипуит фацилис ' +
-    'перицулис не, виси адиписци ад меи. Вел прима интегре те, вениам адверсариум вим цу. ' +
-    'Долорум мнесарчум диссентиунт иус не, но агам примис видерер нец. Еа поссе миним ' +
-    'дицерет сеа, усу менандри цонсулату еи.',
+    'HOME_TITLE': 'Camel Car Hire',
+    'HOME_SUBTITLE': 'Мы не разочаруем вас',
 
     // General
     'GENERAL_ELEM_OBJ': 'Объект №',
@@ -370,61 +250,6 @@ function translateConfig ($translateProvider) {
     'GENERAL_LOADING_SPINNER': 'Информация загружается...',
 
 
-
-    // Exclusive
-    'EXCLUSIVE_PAGE_HEADER_TITLE': 'Эксклюзивные объекты',
-    'EXCLUSIVE_PAGE_HEADER_SUBTITLE': 'Несколько слов...',
-    'EXCLUSIVE_PAGE_BODY_H1': 'Эксклюзивные объекты недвижимости',
-    'EXCLUSIVE_PAGE_BODY_H2': 'Аренда вилл и аппартаментов на Кипре',
-    'EXCLUSIVE_PAGE_BODY_H3': 'Здесь представлена недвижимость, которая находится ' +
-    'в эксклюзивном управлении компании Orange Holliday villas.',
-    'EXCLUSIVE_PAGE_MORE_OBJ': 'Подробная информация об этих и других эксклюзивных объектах',
-    'EXCLUSIVE_PAGE_BUTTON_PRICE': 'Стоимость',
-    'EXCLUSIVE_PAGE_PRICE_MODAL_HEADER': 'Стоимость',
-    'EXCLUSIVE_PAGE_PRICE_MODAL_TABLE_HEAD_COL_1': 'Период',
-    'EXCLUSIVE_PAGE_PRICE_MODAL_TABLE_HEAD_COL_2': 'Стоимость',
-    'EXCLUSIVE_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Закрыть',
-    'EXCLUSIVE_PAGE_BUTTON_CALENDAR': 'Календарь занятости',
-    'EXCLUSIVE_PAGE_CALENDAR_MODAL_HEADER': 'Календарь занятости',
-    'EXCLUSIVE_PAGE_BUTTON_BOOK_OBJECT': 'Забронировать этот объект',
-    'EXCLUSIVE_PAGE_BUTTON_YOUTUBE': 'Посмотреть объект в YouTube',
-    'EXCLUSIVE_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Бронирование объекта',
-
-    // Short term
-    'SHORTTERM_PAGE_HEADER_TITLE': 'Посуточная аренда',
-    'SHORTTERM_PAGE_HEADER_SUBTITLE': 'Несколько слов...',
-    'SHORTTERM_PAGE_MORE_OBJ': 'Подробная информация об этих и других объектах с посуточной арендой',
-    'SHORTTERM_PAGE_BUTTON_PRICE': 'Стоимость',
-    'SHORTTERM_PAGE_PRICE_MODAL_HEADER': 'Стоимость',
-    'SHORTTERM_PAGE_PRICE_MODAL_TABLE_HEAD_COL_1': 'Период',
-    'SHORTTERM_PAGE_PRICE_MODAL_TABLE_HEAD_COL_2': 'Стоимость',
-    'SHORTTERM_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Закрыть',
-    'SHORTTERM_PAGE_BUTTON_CALENDAR': 'Календарь занятости',
-    'SHORTTERM_PAGE_CALENDAR_MODAL_HEADER': 'Календарь занятости',
-    'SHORTTERM_PAGE_BUTTON_BOOK_OBJECT': 'Забронировать этот объект',
-    'SHORTTERM_PAGE_BUTTON_YOUTUBE': 'Посмотреть объект в YouTube',
-    'SHORTTERM_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Бронирование объекта',
-
-
-    // Long term
-    'LONGTERM_PAGE_HEADER_TITLE': 'Долгосрочная аренда',
-    'LONGTERM_PAGE_HEADER_SUBTITLE': 'Несколько слов...',
-    'LONGTERM_PAGE_MORE_OBJ': 'Подробная информация об этих и других объектах с долгосрочной арендой',
-    'LONGTERM_PAGE_BUTTON_BOOK_OBJECT': 'Забронировать этот объект',
-    'LONGTERM_PAGE_BUTTON_YOUTUBE': 'Посмотреть объект в YouTube',
-    'LONGTERM_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Бронирование объекта',
-    'LONGTERM_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Закрыть',
-
-
-
-    // Sale
-    'SALE_PAGE_HEADER_TITLE': 'Продажа',
-    'SALE_PAGE_HEADER_SUBTITLE': 'Несколько слов...',
-    'SALE_PAGE_MORE_OBJ': 'Подробная информация об этих и других объектах продажи',
-    'SALE_PAGE_BUTTON_BOOK_OBJECT': 'Забронировать этот объект',
-    'SALE_PAGE_BUTTON_YOUTUBE': 'Посмотреть объект в YouTube',
-    'SALE_PAGE_BOOK_OBJECT_MODAL_HEADER': 'Бронирование объекта',
-    'SALE_PAGE_MODAL_FOOTER_CLOSE_BUTTON': 'Закрыть',
 
     // Q&A
     'QA_PAGE_HEADER_TITLE': 'Вопросы и ответы',
