afe1d1c0363a7e1ae2d42807149e1e1981613494
dmsch <dschukin@mail.ru>
2017-11-06 23:13:09 +0200

    deploy//update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Deploy bash script..."
 pwd
 cd node_modules/webstudiopro-sails-auth/
 npm i
-cd ..
+cd ../..
 bower i
 rm -rf ./assets/vendor/angular-animate/
 bower i --save-dev angular-animate@1.6.4
