f7b186a303538bd0344a1e457da476f7f1975047
dmsch <dschukin@mail.ru>
2018-07-21 22:37:03 +0300

    update

--- assets/js/app/services/user.service.js
+++ assets/js/app/services/user.service.js
@@ -34,8 +34,8 @@
 
       function successCb(rec) {
 
-        console.log('<<<<<<<<< successCb >>>>>>>>>>>');
-        console.dir(rec);
+        // console.log('<<<<<<<<< successCb >>>>>>>>>>>');
+        // console.dir(rec);
 
         deferred.resolve(rec);
 
@@ -68,8 +68,8 @@
 
       function successCb(rec) {
 
-        console.log('<<<<<<<<< successCb >>>>>>>>>>>');
-        console.dir(rec);
+        // console.log('<<<<<<<<< successCb >>>>>>>>>>>');
+        // console.dir(rec);
 
         deferred.resolve(rec);
 
@@ -103,8 +103,8 @@
 
       function successCb(rec) {
 
-        console.log('<<<<<<<<< successCb >>>>>>>>>>>');
-        console.dir(rec);
+        // console.log('<<<<<<<<< successCb >>>>>>>>>>>');
+        // console.dir(rec);
 
         deferred.resolve(rec);
       } // successCb
@@ -130,8 +130,8 @@
 
       function successCb(rec) {
 
-        console.log('<<<<<<<<< successCb >>>>>>>>>>>');
-        console.dir(rec);
+        // console.log('<<<<<<<<< successCb >>>>>>>>>>>');
+        // console.dir(rec);
 
         deferred.resolve(rec);
       } // successCb
@@ -157,8 +157,8 @@
 
       function successCb(rec) {
 
-        console.log('<<<<<<<<< successCb >>>>>>>>>>>');
-        console.dir(rec);
+        // console.log('<<<<<<<<< successCb >>>>>>>>>>>');
+        // console.dir(rec);
 
         deferred.resolve(rec);
 
